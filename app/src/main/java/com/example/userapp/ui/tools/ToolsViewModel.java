604912ba9e480f5bfe028b0c1dc7cda31aa2569b
eliez <eliezerlancry@hotmail.com>
2020-01-29 17:34:35 +0200

    aaa

--- app/src/main/java/com/example/userapp/ui/tools/ToolsViewModel.java
+++ app/src/main/java/com/example/userapp/ui/tools/ToolsViewModel.java
@@ -1,19 +1,39 @@
 package com.example.userapp.ui.tools;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ToolsViewModel extends ViewModel {
+import com.example.userapp.DAL.MemberName;
+import com.example.userapp.DAL.MemberRepository;
+import com.example.userapp.Entities.Member;
 
-    private MutableLiveData<String> mText;
+import java.util.List;
 
-    public ToolsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is tools fragment");
+public class ToolsViewModel extends AndroidViewModel {
+
+   // private MutableLiveData<String> mText;
+
+    private MemberRepository mRepository;
+    private LiveData<List<Member>> mAllMembers;
+    private LiveData<List<Member>> mMember;
+
+    public ToolsViewModel(Application app) {
+        super(app);
+        //mText = new MutableLiveData<>();
+        //mText.setValue("This is tools fragment");
+        mRepository = new MemberRepository(app);
+        mAllMembers = mRepository.getAllMembers();
+        mMember = mRepository.getMember(MemberName.getMemberAddress());
+
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
+    //public LiveData<String> getText() {
+        //return mText;
+    //}
+    LiveData<List<Member>> getAllMembers() { return mAllMembers; }
+    LiveData<List<Member>> getMember() { return mMember; }
 }
